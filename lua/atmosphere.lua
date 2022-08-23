28712686b5ccea4ac4e994d89dd0dcee5c5c0af3
NULL511 <null511@outlook.com>
2022-08-23 03:41:45 -0400

    initial

---
+++ lua/atmosphere.lua
@@ -0,0 +1,76 @@
+﻿groundRadiusMM = 2.360
+atmosphereRadiusMM = 2.370
+ozoneAbsorptionBase = vec(0.650, 1.881, 0.085)
+mieScatteringBase = 3.996 * 10
+mieAbsorptionBase = 4.4 * 10
+rayleighScatteringBase = vec(5.802, 13.558, 33.1)*2
+rayleighAbsorptionBase = 4.0
+transmittanceSteps = 80
+
+
+function safeacos(x)
+    return acos(clamp(x, -1.0, 1.0))
+end
+
+function rayIntersectSphere(pos, dir, radius)
+    local NoV = dot(pos, dir)
+    local c = dot(pos, pos) - radius^2
+
+    if c > 0.0 and NoV > 0.0 then
+        return -1.0
+    end
+
+    local discr = NoV^2 - c
+    if discr < 0.0 then
+        return -1.0
+    end
+
+    -- Special case: inside sphere, use far discriminant
+    if discr > NoV^2 then
+        return -NoV + sqrt(discr)
+    end
+
+    return -NoV - sqrt(discr)
+end
+
+function getScatteringValues(pos)
+    local altitudeKM = (length(pos) - groundRadiusMM) * 1000.0
+    -- Note: Paper gets these switched up.
+    local rayleighDensity = exp(-altitudeKM / 8.0)
+    local mieDensity = exp(-altitudeKM / 1.2)
+    
+    local rayleighScattering = rayleighScatteringBase * rayleighDensity
+    local rayleighAbsorption = rayleighAbsorptionBase * rayleighDensity
+    
+    local mieScattering = mieScatteringBase * mieDensity
+    local mieAbsorption = mieAbsorptionBase * mieDensity
+    
+    local ozoneAbsorption = ozoneAbsorptionBase * max(0.0, 1.0 - abs(altitudeKM - 25.0) / 15.0)
+    
+    local extinction = rayleighScattering + rayleighAbsorption + mieScattering + mieAbsorption + ozoneAbsorption
+
+    return rayleighScattering, mieScattering, extinction
+end
+
+function getSunTransmittance(pos, sunDir)
+    if rayIntersectSphere(pos, sunDir, groundRadiusMM) > 0.0 then
+        return vec(0.0, 0.0, 0.0)
+    end
+    
+    local atmoDist = rayIntersectSphere(pos, sunDir, atmosphereRadiusMM)
+    local transmittance = vec(1.0, 1.0, 1.0)
+    local t = 0.0
+
+    for i = 0,transmittanceSteps,1 do
+        local newT = ((i + 0.3) / transmittanceSteps) * atmoDist
+        local dt = newT - t
+        t = newT
+        
+        local newPos = pos + t*sunDir
+        local rayleighScattering, mieScattering, extinction = getScatteringValues(newPos)
+        
+        transmittance = transmittance * exp(-dt*extinction)
+    end
+
+    return transmittance
+end
---
+++ lua/multiple_scattering.lua
@@ -0,0 +1,135 @@
+﻿-- Buffer B is the multiple-scattering LUT. Each pixel coordinate corresponds to a height and sun zenith angle, and
+-- the value is the multiple scattering approximation (Psi_ms from the paper, Eq. 10).
+local groundAlbedo = vec(0.3, 0.3, 0.3)
+local mulScattSteps = 4.0 --20
+local sqrtSamples = 8.0
+
+
+function getSphericalDir(theta, phi)
+    local cosPhi = cos(phi)
+    local sinPhi = sin(phi)
+    local cosTheta = cos(theta)
+    local sinTheta = sin(theta)
+    return vec(sinPhi * sinTheta, cosPhi, sinPhi * cosTheta)
+end
+
+function getMiePhase(cosTheta)
+    local g = 0.8
+    local scale = 3.0 / (8.0 * PI)
+    
+    local num = (1.0 - g^2) * (1.0 + cosTheta^2)
+    local denom = (2.0 + g^2) * (1.0 + g^2 - 2.0*g*cosTheta)^1.5
+    
+    return scale * num / denom
+end
+
+function getRayleighPhase(cosTheta)
+    local k = 3.0 / (16.0 * PI)
+    return k * (1.0 + cosTheta^2)
+end
+
+function getValFromTLUT(pos, sunDir)
+    --local height = length(pos);
+    --local up = pos / height;
+    --local sunCosZenithAngle = dot(sunDir, up);
+
+    --local uv = vec2(tLUTRes.x*clamp(0.5 + 0.5*sunCosZenithAngle, 0.0, 1.0),
+    --               tLUTRes.y*max(0.0, min(1.0, (height - groundRadiusMM)/(atmosphereRadiusMM - groundRadiusMM))));
+
+    --uv = uv / bufferRes;
+    return getSunTransmittance(pos, sunDir)
+end
+
+-- Calculates Equation (5) and (7) from the paper.
+function getMulScattValues(pos, sunDir)
+    local lumTotal = vec(0.0, 0.0, 0.0)
+    local fms = vec(0.0, 0.0, 0.0)
+    
+    local invSamples = rcp(sqrtSamples^2)
+    for i = 0.0,sqrtSamples,1.0 do
+        for j = 0.0,sqrtSamples,1.0 do
+            -- This integral is symmetric about theta = 0 (or theta = PI), so we
+            -- only need to integrate from zero to PI, not zero to 2*PI.
+            local theta = PI * (i + 0.5) / sqrtSamples
+            local phi = safeacos(1.0 - 2.0*(j + 0.5) / sqrtSamples)
+            local rayDir = getSphericalDir(theta, phi)
+            
+            local atmoDist = rayIntersectSphere(pos, rayDir, atmosphereRadiusMM)
+            local groundDist = rayIntersectSphere(pos, rayDir, groundRadiusMM)
+
+            local tMax = atmoDist
+            if groundDist > 0.0 then
+                tMax = groundDist
+            end
+            
+            local cosTheta = dot(rayDir, sunDir)
+    
+            local miePhaseValue = getMiePhase(cosTheta)
+            local rayleighPhaseValue = getRayleighPhase(-cosTheta)
+            
+            local lum = vec(0.0, 0.0, 0.0)
+            local lumFactor = vec(0.0, 0.0, 0.0)
+            local transmittance = vec(1.0, 1.0, 1.0)
+            local t = 0.0
+
+            for stepI = 0.0,mulScattSteps,1.0 do
+                local newT = ((stepI + 0.3) / mulScattSteps) * tMax
+                local dt = newT - t
+                t = newT
+
+                local newPos = pos + t*rayDir
+                local rayleighScattering, mieScattering, extinction = getScatteringValues(newPos)
+                local sampleTransmittance = exp(-dt*extinction)
+                
+                -- Integrate within each segment.
+                local scatteringNoPhase = rayleighScattering + mieScattering
+                local scatteringF = (scatteringNoPhase - scatteringNoPhase * sampleTransmittance) / extinction
+                lumFactor = lumFactor + transmittance * scatteringF
+                
+                -- This is slightly different from the paper, but I think the paper has a mistake?
+                -- In equation (6), I think S(x,w_s) should be S(x-tv,w_s).
+                local sunTransmittance = getValFromTLUT(newPos, sunDir)
+
+                local rayleighInScattering = rayleighScattering * rayleighPhaseValue
+                local mieInScattering = mieScattering * miePhaseValue
+                local inScattering = (rayleighInScattering + mieInScattering) * sunTransmittance
+
+                -- Integrated scattering within path segment.
+                local scatteringIntegral = (inScattering - inScattering * sampleTransmittance) / extinction
+
+                lum = lum + scatteringIntegral * transmittance
+                transmittance = transmittance * sampleTransmittance
+            end
+            
+            if groundDist > 0.0 then
+                local hitPos = pos + groundDist*rayDir
+                if dot(pos, sunDir) > 0.0 then
+                    hitPos = normalize(hitPos) * groundRadiusMM
+                    lum = lum + transmittance * groundAlbedo * getValFromTLUT(hitPos, sunDir)
+                end
+            end
+            
+            fms = fms + lumFactor * invSamples
+            lumTotal = lumTotal + lum * invSamples
+        end
+    end
+
+    return lumTotal, fms
+end
+
+function processPixel(x, y)
+    local u = (x + 0.5) / imageWidth
+    local v = (y + 0.5) / imageHeight
+
+    local sunCosTheta = 2.0*u - 1.0
+    local sunTheta = safeacos(sunCosTheta)
+    local height = mix(groundRadiusMM, atmosphereRadiusMM, v)
+    
+    local pos = vec(0.0, height, 0.0)
+    local sunDir = normalize(vec(0.0, sunCosTheta, -sin(sunTheta)))
+    
+    local lum, f_ms = getMulScattValues(pos, sunDir);
+    
+    -- Equation 10 from the paper.
+    return lum  / (1.0 - f_ms); 
+end
