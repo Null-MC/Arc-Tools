a0e015194b3b3c45793a8187f6985caf96b391cb
NULL511 <null511@outlook.com>
2022-12-10 02:37:40 -0500

    cleanup

--- lua/clouds_3d.lua
+++ lua/clouds_3d.lua
@@ -1,62 +1,51 @@
 local SEED = 467456
+local SCALE = 16.0
 
-function easeIn(interpolator)
-    return interpolator^2
+function hash14(p4)
+    p4 = fract(p4 * vec(0.1031, 0.1030, 0.0973, 0.1099))
+    p4 = p4 + dot(p4, p4.wzxy + 33.33)
+    return fract((p4.x + p4.y) * (p4.z + p4.w))
 end
 
-function easeOut(interpolator)
-    return 1.0 - easeIn(1.0 - interpolator)
+function tileable3DNoise(coord, seed, wrap)
+    local fractCoord  = fract(coord)
+    local floorCoord  = mod(coord - fractCoord, wrap)
+    local ceilCoord   = mod(floorCoord + 1.0, wrap)
+    local smoothCoord = smoothstep(0.0, 1.0, fractCoord)
+    
+    return mix(
+        mix(
+            mix(
+                hash14(vec(floorCoord.x, floorCoord.y, floorCoord.z, seed)),
+                hash14(vec(floorCoord.x, floorCoord.y,  ceilCoord.z, seed)),
+                smoothCoord.z
+            ),
+            mix(
+                hash14(vec(floorCoord.x,  ceilCoord.y, floorCoord.z, seed)),
+                hash14(vec(floorCoord.x,  ceilCoord.y,  ceilCoord.z, seed)),
+                smoothCoord.z
+            ),
+            smoothCoord.y
+        ),
+        mix(
+            mix(
+                hash14(vec( ceilCoord.x, floorCoord.y, floorCoord.z, seed)),
+                hash14(vec( ceilCoord.x, floorCoord.y,  ceilCoord.z, seed)),
+                smoothCoord.z
+            ),
+            mix(
+                hash14(vec( ceilCoord.x,  ceilCoord.y, floorCoord.z, seed)),
+                hash14(vec( ceilCoord.x,  ceilCoord.y,  ceilCoord.z, seed)),
+                smoothCoord.z
+            ),
+            smoothCoord.y
+        ),
+        smoothCoord.x
+    )
 end
 
-function easeInOut(interpolator)
-    local easeInValue = easeIn(interpolator)
-    local easeOutValue = easeOut(interpolator)
-    return mix(easeInValue, easeOutValue, interpolator)
-end
-
-function rand3dTo1d(value, dotDir)
-    local smallValue = sin(value)
-    local random = dot(smallValue, dotDir)
-    return fract(sin(random) * 143758.5453)
-end
-
-function rand3dTo3d(value)
-    local v1 = rand3dTo1d(value, vec(12.989, 78.233, 37.719))
-    local v2 = rand3dTo1d(value, vec(39.346, 11.135, 83.155))
-    local v3 = rand3dTo1d(value, vec(73.156, 52.235, 09.151))
-
-    return vec(v1, v2, v3)
-end
-
-function perlinNoise(value)
-    local fraction = fract(value)
-    local interpolator = easeInOut(fraction)
-
-    local cellNoiseZ = {}
-
-    for z = 0,1 do
-        local cellNoiseY = {}
-
-        for y = 0,1 do
-            local cellNoiseX = {}
-
-            for x = 0,1 do
-                local cell = floor(value) + vec(x, y, z)
-                local cellDirection = rand3dTo3d(cell) * 2.0 - 1.0
-                local compareVector = fraction - vec(x, y, z)
-                cellNoiseX[x] = dot(cellDirection, compareVector)
-            end
-
-            cellNoiseY[y] = mix(cellNoiseX[0], cellNoiseX[1], interpolator.x)
-        end
-
-        cellNoiseZ[z] = mix(cellNoiseY[0], cellNoiseY[1], interpolator.y)
-    end
-
-    return mix(cellNoiseZ[0], cellNoiseZ[1], interpolator.z)
-end
 
 function processTexel(x, y, z)
     local pos = vec(x, y, z)
-    return tileable3DNoise(pos * 16.0, SEED, vec(16, 16, 16))
+    return tileable3DNoise(pos * SCALE, SEED, vec(SCALE, SCALE, SCALE))
 end
